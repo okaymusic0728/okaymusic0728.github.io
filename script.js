6ead7dc02c848c28c08d80a84f2f82958eb47809
okaymusic0728 <yoshi_nori_134r@yahoo.co.jp>
2026-08-02 16:39:39 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,45 @@
-function playSong(file) {
+const songs = [
+  {
+    title: "01. 希望",
+    file: "01.mp3"
+  },
+  {
+    title: "02. 涙の正体",
+    file: "02.mp3"
+  },
+  {
+    title: "03. 燦然",
+    file: "03.mp3"
+  }
+];
+
+
+let currentSong = 0;
+
+
+function playSong(number) {
 
   const player = document.getElementById("player");
 
-  player.src = "audio/" + file;
+  currentSong = number;
+
+  player.src = songs[number].file;
 
   player.play();
 
 }
+
+
+document
+.getElementById("player")
+.addEventListener(
+"ended",
+function(){
+
+  if(currentSong < songs.length - 1){
+
+    playSong(currentSong + 1);
+
+  }
+
+});
